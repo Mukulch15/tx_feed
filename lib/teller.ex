9863edd5ee2f800d55b761ef628a79aab3b15985
Mukul Chakravarty <mukul.ch15@gmail.com>
2021-12-27 21:33:54 +0530

    updated docs

--- lib/teller.ex
+++ lib/teller.ex
@@ -1,9 +1,30 @@
 defmodule Teller do
   @moduledoc """
-  Teller keeps the contexts that define your domain
-  and business logic.
+    This application acts as a sandbox for teller bank apis. Aim of the application is to mimick accounts and transaction details
+    so that developers can test these apis on their interface without the need for any actual bank accounts.
+    The application gives access to the following apis:
+      * GET /accounts
+      * GET /accounts/:account_id
+      * GET /accounts/:account_id/details
+      * GET /accounts/:account_id/balances
+      * GET /accounts/:account_id/transactions
+      * GET /accounts/:account_id/transactions/:transaction_id
 
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
+    To start your Phoenix server:
+
+    * Install dependencies with `mix deps.get`
+    * Start Phoenix endpoint with `mix phx.server` or inside IEx with `iex -S mix phx.server`
+    In order to start using the apis, you have to use basic auth. Enter the username in the following format:
+    test_ followed by a 12 digit number. The password must be blank.
+    The base url is http://localhost:4000.
+    A dashboard is accessible on http://localhost:4000/statistics which gives you the count of api calls for a given token.
+
+    Technical specification:
+      * The api is accessed using a token in the following format test_456789012332.
+      * The 12 digit random number acts as a seed for the account numbers accessible through the given api token.
+      * Example in the above case the accounts with numbers 456789012332 and 456789012333 will be generated and accessible.
+      * The transaction feed is generated for 90 days from the current date and will give you the same data for each token and account id each
+        and everytime. Even after app restarts.
+      * The dashboard is powered by live view and phoenix pubsub.
   """
 end
